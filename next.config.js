db8a6e37db37fcb03089ae5b28b288bd3985cc22
TalkingRaven0 <arwinsalazar@gmail.com>
2024-03-27 12:27:32 +0800

    moved to vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const env = process.env.NODE_ENV
 
-var nextConfig = {
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-  };
-
-if (env == "production"){
-    nextConfig['basePath'] = "/git-portfolio";
-    nextConfig['assetPrefix'] = "/git-portfolio/";
-}
+var nextConfig = {};
   
   module.exports = nextConfig;
