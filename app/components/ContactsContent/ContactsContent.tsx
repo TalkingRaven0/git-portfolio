dc018659202b8bb9d3871100862ab52031347b46
TalkingRaven0 <arwinsalazar@gmail.com>
2024-04-19 22:01:36 +0800

    adjusted contact me alignments

--- app/components/ContactsContent/ContactsContent.tsx
+++ app/components/ContactsContent/ContactsContent.tsx
@@ -14,9 +14,11 @@ const ContactsContent = () => {
   return (
     <div className='center-shadow flex-none flex h-fit bg-sky-950 border-t-2 border-cyan-400 px-10 py-3 font-mono text-sky-200'>
       <div className='flex-[1] font-mono text-3xl text-center content-center drop-shadow-glow text-sky-300'>CONTACT ME:</div>
-      <div className='flex-[2] h-fit grid grid-cols-3 gap-1 '>
-        <div className='text-blue-300'>Full Name:         </div> <div className=' col-span-2'>Den Arwin G. Salazar</div>
-        <div className='text-blue-300'>Current Job Title: </div> <div className=' col-span-2'>Junior Graduate Developer</div>
+      <div className='flex-[2] flex h-full'>
+        <div className='grid grid-cols-3 gap-1 my-auto w-full'>
+          <div className='text-blue-300'>Full Name:         </div> <div className=' col-span-2'>Den Arwin G. Salazar</div>
+          <div className='text-blue-300'>Current Job Title: </div> <div className=' col-span-2'>Junior Graduate Developer</div>
+        </div>
       </div>
       <div className='flex-[2] grid grid-cols-3 gap-1'>
         <div className='text-blue-300' >Email Address:   </div> <div className=' col-span-2 underline drop-shadow-glow text-sky-400'> <a ref={emailRef} target="_blank" href='mailto:densalazar1022@gmail.com' onClick={()=>copyToClipboard(emailRef)}>densalazar1022@gmail.com</a></div>
