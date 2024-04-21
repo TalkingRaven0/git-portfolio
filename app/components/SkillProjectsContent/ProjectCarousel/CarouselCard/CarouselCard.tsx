e4329732d68fd0d111669dd5ad1f82e8d7b164b2
TalkingRaven0 <arwinsalazar@gmail.com>
2024-04-21 16:19:46 +0800

    animated project carousel

--- app/components/SkillProjectsContent/ProjectCarousel/CarouselCard/CarouselCard.tsx
+++ app/components/SkillProjectsContent/ProjectCarousel/CarouselCard/CarouselCard.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { forwardRef } from 'react'
 
-const CarouselCard = ({extraClasses,child}:{extraClasses:string, child:React.JSX.Element}) => {
+const CarouselCard = forwardRef(function CarouselCard({className,child}:{className:string, child:React.JSX.Element},ref:any) {
   return (
-    <div className={' w-[50vw] bg-slate-900 border-2 border-sky-400'+extraClasses}>
+    <div ref={ref} className={'absolute-centered w-[50vw] h-full bg-slate-900 border-2 border-sky-400 '+ className}>
         {child}
     </div>
   )
-}
+});
 
 export default CarouselCard
--- app/components/SkillProjectsContent/ProjectCarousel/ProjectCarousel.tsx
+++ app/components/SkillProjectsContent/ProjectCarousel/ProjectCarousel.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import React, { useState } from 'react'
+import React, {useMemo, useRef, useState } from 'react'
 import CarouselCard from './CarouselCard/CarouselCard';
 import '@/app/customStyles/projectStyle.css'
 
@@ -7,25 +7,91 @@ const ProjectCarousel = ({children}: {children:Array<React.JSX.Element>}) => {
     const [currentIndex,currentIndexSet] = useState(0);
 
     const centerDisplay = children[currentIndex];
-    const leftDisplay = (currentIndex==0) ? children[children.length-1] : children[currentIndex-1];
-    const rightDisplay = (currentIndex >= children.length-1) ? children[0] : children[currentIndex+1];
+    let movement = 0;
 
-    function nextProject(){
+    const refLeftHidden = useRef<null|HTMLDivElement>(null)
+    const refLeftDisplay = useRef<null|HTMLDivElement>(null)
+    const refCenterDisplay = useRef<null|HTMLDivElement>(null)
+    const refRightDisplay = useRef<null|HTMLDivElement>(null)
+    const refRightHidden = useRef<null|HTMLDivElement>(null)
+
+    const leftDisplay = (currentIndex==0) ?
+    children[children.length-1] : 
+    children[currentIndex-1];
+
+    const leftHidden = (currentIndex-2 < 0) ?
+    children[children.length-1] :
+    children[currentIndex-2];
+
+    const rightDisplay = (currentIndex >= children.length-1) ?
+    children[0] :
+    children[currentIndex+1];
+
+    const rightHidden = (currentIndex+1 >= children.length-1) ?
+    (currentIndex+1 == children.length-1) ? children[0]:children[1] :
+    children[currentIndex+2];
+
+    function nextProject(){ // function will only start the animations
+        if(refLeftDisplay.current == null) return;
+        if(refCenterDisplay.current == null) return;
+        if(refRightDisplay.current == null) return;
+        if(refRightHidden.current == null) return;
+
+        refRightDisplay.current.classList.add("rNext");
+        refCenterDisplay.current.classList.add("cNext");
+        refRightHidden.current.classList.add("rhNext");
+        refLeftDisplay.current.classList.add("lNext");
+        movement = 1;
+    }
+
+    function previousProject(){ // function will only start the animations
+        if(refLeftHidden.current == null) return;
+        if(refLeftDisplay.current == null) return;
+        if(refCenterDisplay.current == null) return;
+        if(refRightDisplay.current == null) return;
+        
+        refRightDisplay.current.classList.add("rPrev");
+        refCenterDisplay.current.classList.add("cPrev");
+        refLeftHidden.current.classList.add("lhPrev");
+        refLeftDisplay.current.classList.add("lPrev");
+        movement = -1;
+
+    }
+    useMemo(()=>{
+        if(refRightDisplay.current==null)return;
+        if(refCenterDisplay.current==null)return;
+        if(refRightHidden.current==null)return;
+        if(refLeftDisplay.current==null)return;
+        if(refLeftHidden.current==null)return;
+
+        refLeftDisplay.current.classList.remove("lPrev");
+        refRightDisplay.current.classList.remove("rPrev");
+        refCenterDisplay.current.classList.remove("cPrev");
+        refLeftHidden.current.classList.remove("lhPrev");
+
+        refLeftDisplay.current.classList.remove("lNext");
+        refRightDisplay.current.classList.remove("rNext");
+        refCenterDisplay.current.classList.remove("cNext");
+        refRightHidden.current.classList.remove("rhNext");
+    },[currentIndex])
+
+    if(refRightDisplay.current!=null) refRightDisplay.current.onanimationend=() => {
+        if(movement != 1) return;
         if(currentIndex >= children.length-1){
             currentIndexSet(0);
         } else {
             currentIndexSet(currentIndex+1);
         }
     }
-
-    function previousProject(){
+    
+    if(refLeftDisplay.current!=null) refLeftDisplay.current.onanimationend = () => {
+        if(movement != -1) return;
         if(currentIndex == 0){
             currentIndexSet(children.length-1);
         } else {
             currentIndexSet(currentIndex-1);
         }
     }
-
     return (
         <div className='h-full relative'>
             <button className=' absolute z-50 right-0 h-full bg-black bg-opacity-15 hover:bg-slate-600 hover:bg-opacity-20' onClick={nextProject}>
@@ -36,10 +102,12 @@ const ProjectCarousel = ({children}: {children:Array<React.JSX.Element>}) => {
             </button>
 
             <div className='overflow-clip opacity-gradient-tr h-full'>
-                <div className=' flex flex-row w-fit h-full py-3 relative center left-1/2 -translate-x-1/2'>
-                    <CarouselCard extraClasses=' scale-75 -mr-52 -z-10' child={leftDisplay} />
-                    <CarouselCard extraClasses=' center-shadow z-10' child={centerDisplay} />
-                    <CarouselCard extraClasses=' scale-75 -ml-52 -z-10' child={rightDisplay} />
+                <div className='absolute-centered w-full h-full py-3 relative'>
+                    <CarouselCard ref={refLeftHidden} className =' translate-x-[-140%] scale-50 -z-20' child={leftHidden} />
+                    <CarouselCard ref={refLeftDisplay} className =' translate-x-[-105%] scale-75 -z-10' child={leftDisplay} />
+                    <CarouselCard ref={refCenterDisplay} className =' center-shadow z-10' child={centerDisplay} />
+                    <CarouselCard ref={refRightDisplay} className =' translate-x-[15%] scale-75 -z-10' child={rightDisplay} />
+                    <CarouselCard ref={refRightHidden} className = ' translate-x-[50%] scale-50 -z-20' child={rightHidden} />
                 </div>
             </div>
         </div>
