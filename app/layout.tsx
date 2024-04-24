142224348cb0ef4820e11957163865bbbe25bf44
TalkingRaven0 <arwinsalazar@gmail.com>
2024-04-24 09:28:29 +0800

    metadata modifications

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Den Salazar's Portfolio",
-  description: "Made with Next JS",
+  description: "Who am I, What do I do, and What Can I do."
 };
 
 export default function RootLayout({
@@ -16,6 +16,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://den-salazar-portfolio.vercel.app/" />
+          <meta property="og:title" content="Den Salazar's Portfolio" />
+          <meta property="og:description" content="Who am I, What do I do, and What Can I do." />
+          <meta property="og:image" content="/resources/thumbnail.png" />
+
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://den-salazar-portfolio.vercel.app/" />
+          <meta property="twitter:title" content="Den Salazar's Portfolio" />
+          <meta property="twitter:description" content="Who am I, What do I do, and What Can I do." />
+          <meta property="twitter:image" content="/resources/thumbnail.png" />
+
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
